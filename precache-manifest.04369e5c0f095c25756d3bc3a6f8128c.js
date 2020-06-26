1ab7df376425ba52af62940dc43bd7e4a8aa507f
amie-affection <amie.affection@gmail.com>
2020-06-27 02:27:32 +0300

    Updates

---
+++ precache-manifest.04369e5c0f095c25756d3bc3a6f8128c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a04946b5cb91138dc295dda73b0747",
+    "url": "/Wallet/index.html"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/Wallet/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Wallet/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56da230e4ee40719cae3",
+    "url": "/Wallet/static/js/main.286961d2.chunk.js"
+  },
+  {
+    "revision": "1f8ea9a0ce932eb3e03b",
+    "url": "/Wallet/static/js/runtime-main.363a5216.js"
+  }
+]);
